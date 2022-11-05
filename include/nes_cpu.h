4abe515b1bbff51fc3b9d7a10e0af4ee40742f4e
ArtemLeshch <artemleshchukov02@gmail.com>
2022-11-05 08:03:53 +0400

    added flag enum

--- include/nes_cpu.h
+++ include/nes_cpu.h
@@ -13,10 +13,26 @@ public:
 
 	void connectBus(Bus* b) { bus = b;  }
 
+public:
+	enum FLAGS6502
+	{
+		C = (1 << 0),	// Carry bit
+		Z = (1 << 1),	// Zero
+		I = (1 << 2),	// Disable Interrupts
+		D = (1 << 3),	// Decimal mode (not used in this implementation)
+		B = (1 << 4),	// Break
+		U = (1 << 5),	// Unused
+		V = (1 << 6),	// Overflow
+		N = (1 << 7)	// Negative
+	};
+
+
+
 private:
 	Bus* bus = nullptr;
 	void write(uint16_t addr, uint8_t data);
 	uint8_t read(uint16_t addr);
+
 };
 
 
